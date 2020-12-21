85434bf1c31a2fb6ee9482c2db3f6163c22ae919
Daniel Moreira <danieljt84@gmail.com>
2020-12-21 16:13:08 -0300

    Cache adicionado

--- forum/src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
+++ forum/src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import br.com.alura.forum.models.Topico;
 
 public class TopicoDto {
@@ -33,8 +35,8 @@ public class TopicoDto {
 		return dataCriacao;
 	}
 
-	public static List<TopicoDto> converte(List<Topico> topicos) {
+	public static Page<TopicoDto> converte(Page<Topico> topicos) {
 		// TODO Auto-generated method stub
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+		return topicos.map(TopicoDto::new);
 	}
 }
