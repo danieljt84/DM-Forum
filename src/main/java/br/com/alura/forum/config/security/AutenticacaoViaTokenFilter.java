69270cb346df8985f121f784c74ff88dc4e9b59a
Daniel Moreira <danieljt84@gmail.com>
2020-12-28 20:33:42 -0300

    validando o token

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -12,12 +12,19 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 
+	private TokenService tokenService;
+	
+	public AutenticacaoViaTokenFilter(TokenService tokenService) {
+		this.tokenService= tokenService;
+	}
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
 		String token = recuperarToken(request);
-		System.out.println(token);
+		
+		boolean valido = tokenService.isValidoToken(token);
+		System.out.println(valido);
 		filterChain.doFilter(request, response);
 	}
 	
